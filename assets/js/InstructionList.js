3ba948f71ad4900b4cf373869b1585d9d87f7ecc
McKendree Strommer <mckendree.strommer@gmail.com>
2021-07-31 00:02:33 -0700

    started index.js, connected InstructionList class to the inquirer prompts, now I need to flesh out the functions

---
+++ assets/js/InstructionList.js
@@ -0,0 +1,32 @@
+class InstructionList {
+    constructor(database) {
+        this.db = database;
+    }
+
+    viewAllDepartments() {
+
+    } 
+    viewAllRoles() {
+
+    } 
+    viewAllEmployees() {
+
+    } 
+    addDepartment() {
+
+    }
+    addRole() {
+
+    }
+    addEmployee() {
+
+    }
+    updateEmployeeRole() {
+
+    }
+    exit() {
+        console.log('goodbye!');
+    }
+}
+
+module.exports = InstructionList;
--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-const db = require('./assets/config/sqlConfig');
-
-console.log('made it here!');
